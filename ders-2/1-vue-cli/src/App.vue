65f8df09df456ad41ff730b49e657e8c56aa85c5
tugbabogaoglu <Tuba0607>
2021-11-20 12:07:43 +0300

    bootcamp ders-2

---
+++ ders-2/1-vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <app-header />
+  <br>
+  <br>
+  <div class="container">
+    <h3>Bu benim ilk Vue CLI Uygulamam</h3>
+    <p>Bugün component ve Vue CLI konularını görüyoruz..</p>
+  </div>
+  <counter-item />
+  <counterItem />
+</template>
+<script>
+import CounterItem from "@/components/CounterItem";
+export default {
+  components: {
+    CounterItem,
+  },
+};
+</script>
