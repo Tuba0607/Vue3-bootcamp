35102cccffc45bd3c55da1975bfe42a231a2edd2
Tuba0607 <bogaoglutugba@gmail.com>
2021-12-22 23:31:18 +0300

    Ders 3 eklendi

---
+++ ders-3/2-dynamic-components/src/components/Blue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3 class="bg-blue text-white">Blue Component</h3>
+    <label>
+      <input type="text" />
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    console.log("BLUE mounted");
+  },
+  activated() {
+    console.log("BLUE activated");
+  },
+  deactivated() {
+    console.log("BLUE deactivated");
+  }
+};
+</script>
